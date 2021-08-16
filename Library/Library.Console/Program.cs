1df12bfc1acc0f15f76921c43a45eedad4d97aa0
lalmeida <leandro.almeida@inflor.com>
2021-08-16 11:36:44 -0300

    codificando librarys

---
+++ Library/Library.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using Library.DataHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Library.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertUsuario();
+
+            var dt = new DataTable();
+
+            var cmd = "sp_ListaUsuario";
+
+            dt = (DataTable)GenericDataBase.ExecuteCommand(cmd, CommandType.StoredProcedure, null, TypeCommand.ExecuteDataTable);
+
+            foreach (DataRow item in dt.Rows)
+            {
+                System.Console.WriteLine("O Usuário é o {0} que mora na Rua {1}", item["Nome"], item["Endereco"]);
+            }
+
+
+            System.Console.ReadKey();
+        }
+
+        static void InsertUsuario()
+        {
+            var query = string.Format("INSERT INTO Usuario(Nome, Endereco) VALUES('{0}', '{1}')", "Leandro", "Av. castro Alves");
+
+            GenericDataBase.ExecuteCommand(query, CommandType.Text, null, TypeCommand.ExecuteNonQuery);
+
+        }
+
+    }
+}
---
+++ Library/Library.DataHelper/GenericDataBase.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data;
+
+namespace Library.DataHelper
+{
+    public class GenericDataBase
+    {
+
+        public static DbCommand CreateCommand(string cmdText, CommandType cmnType, List<DbParameter> listParameters)
+        {
+            var factory = DbProviderFactories.GetFactory(ConnectionDB.ProviderName);
+
+            var conn = factory.CreateConnection();
+
+            conn.ConnectionString = ConnectionDB.ConnectionString;
+
+            var comn = conn.CreateCommand();
+
+            comn.CommandText = cmdText;
+
+            comn.CommandType = cmnType;
+
+            if (listParameters != null)
+            {
+                foreach(var param in listParameters)
+                {
+                    comn.Parameters.Add(param);
+                }
+            }
+
+            return comn;
+
+
+        }
+
+        public static DbParameter CreateParameter(string nameParameter, DbType typeParameter, Object valueParameter)
+        {
+            var factory = DbProviderFactories.GetFactory(ConnectionDB.ProviderName);
+
+            var param = factory.CreateParameter();
+
+            if (param != null)
+            {
+                param.ParameterName = nameParameter;
+                param.DbType = typeParameter;
+                param.Value = valueParameter;
+            }
+
+            return param;
+        }
+
+        public static Object ExecuteCommand(string cmdText, CommandType cmdType, List<DbParameter> listParameter, TypeCommand typeCmd)
+        {
+
+            var command = CreateCommand(cmdText, cmdType, listParameter);
+            Object Objretorno = null;
+
+            try
+            {
+                command.Connection.Open();
+
+                switch (typeCmd)
+                {
+                    case TypeCommand.ExecuteNonQuery:
+                        Objretorno = command.ExecuteNonQuery();
+                        break;
+                    case TypeCommand.ExecuteReader:
+                        Objretorno = command.ExecuteReader();
+                        break;
+                    case TypeCommand.ExecuteScalar:
+                        Objretorno = command.ExecuteScalar();
+                        break;
+                    case TypeCommand.ExecuteDataTable:
+                        var table = new DataTable();
+                        var reader = command.ExecuteReader();
+
+                        table.Load(reader);
+
+                        reader.Close();
+                        Objretorno = table;
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Erro ao executar Execute Comando");
+            }
+            finally
+            {
+                if (typeCmd != TypeCommand.ExecuteReader)
+                {
+                    if (command.Connection.State == ConnectionState.Open)
+                    {
+                        command.Connection.Close();
+                    }
+                    command.Connection.Dispose();
+                }
+            }
+
+            return Objretorno;
+
+        }
+
+    }
+
+    public enum TypeCommand
+    {
+        ExecuteNonQuery,
+        ExecuteReader,
+        ExecuteScalar,
+        ExecuteDataTable
+    }
+}
